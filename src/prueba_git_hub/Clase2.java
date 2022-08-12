fcbbbb4c8668fe2611996a41e401179bfb2038e6
Walther Carrasco <110209326+walthercarrasco@users.noreply.github.com>
2022-08-12 12:07:58 -0600

    Nomas

--- src/prueba_git_hub/Clase2.java
+++ src/prueba_git_hub/Clase2.java
@@ -10,5 +10,37 @@ package prueba_git_hub;
  * @author emili
  */
 public class Clase2 {
+    String nombre;
+    int edad;
+
+    public Clase2() {
+    }
+
+    public Clase2(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase2{" + "nombre=" + nombre + ", edad=" + edad + '}';
+    }
+    
     
 }
