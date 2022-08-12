dd40dd7f577d48db891f8d6d42b1da23d03d7649
Walther Carrasco <110209326+walthercarrasco@users.noreply.github.com>
2022-08-12 11:50:32 -0600

    Hola

--- src/prueba_git_hub/Clase2.java
+++ src/prueba_git_hub/Clase2.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package prueba_git_hub;
 
 /**
@@ -10,5 +6,47 @@ package prueba_git_hub;
  * @author emili
  */
 public class Clase2 {
+    String nombre;
+    int edad;
+    int altura;
+
+    public Clase2() {
+    }
+
+    public Clase2(String nombre, int edad, int altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.altura = altura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase2{" + "nombre=" + nombre + ", edad=" + edad + ", altura=" + altura + '}';
+    }
+    
     
 }
